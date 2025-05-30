bc25f96691bdf193512d90ddd4f5ef996fb8398b
Adefisayomi Ariyo <ariyoadefisayomi@gmail.com>
2025-05-30 07:32:41 -0400

    adding new uncompleted excercises

---
+++ datatypes/ex1.py
@@ -0,0 +1,12 @@
+#imported these functions and variables
+
+def break_words(stuff):
+    '''This function will break up words for us.'''
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    '''Sorts the words.'''
+    return sorted(words)
+
+def s
