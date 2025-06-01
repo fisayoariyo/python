46fb0b7622048779a0e679686dfd29b3cdb0818e
Adefisayomi Ariyo <ariyoadefisayomi@gmail.com>
2025-06-01 10:27:32 -0400

    More excercies on Variable names

---
+++ diversion/ex5.py
@@ -0,0 +1,20 @@
+# More variables and printing
+
+me = 'Zed A. Shaw'
+me_age = 35 #not a lie
+me_height = 74 #inches
+me_weight = 180 #lbs
+me_eyes = 'Blue'
+me_teeth = 'White'
+me_hair = 'Brown'
+print(f"Let's talk about {me}.")
+print(f"He's {me_height} inches tall.")
+print(f"He's {me_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {me_eyes} eyes and {me_hair} hair.")
+print(f"His teeth are usually {me_teeth} depending on the coffee.")
+
+#this line is tricky, try to get it exactly right
+total = me_age + me_height + me_weight
+print(f"If I add {me_age}, {me_height}, and {me_weight} I get {total}.")
+
---
+++ diversion/ex5_1.py
@@ -0,0 +1,7 @@
+# This will convert inches and  pounds  to centimeters and kilograms
+
+1_cm = 0.3937 inches
+1_kg = 2.2046 pounds
+
+print("")
+
